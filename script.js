c5a28eb30d27829f6c024319282b43d933f0e496
Rebecca D <rdolmon08@gmail.com>
2023-06-29 14:23:12 -0500

    finished css, adding js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //https://www.youtube.com/watch?v=wtqbOFYivxw&list=PLtMugc7g4GaqAVDZwQ_t1H6500ZGJzOgW&index=11&ab_channel=WebDevTutorials
+
+const flashcards = document.getElementsByClassName("flashcards")[0]; 
+const createBox = document.getElementsByClassName("create-box")[0]; 
+const question = document.getElementById("question");
+const answer = document.getElementById("answer");
